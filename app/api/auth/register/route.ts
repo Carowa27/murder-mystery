290830aca49fd9555a0dc201b543d8d2a153a93c
StevenLomon <steven.cesario@proton.me>
2026-09-18 14:37:53 +0200

    request body with variables that might cha nge

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,2 +1,7 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, password } = body; // Bör diskuteras i gruppen ifall mer ska frågas efter vid sign up! Email och password får now, kan komma att ändras
+}
