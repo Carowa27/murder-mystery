9f7fe66b3c346580507c8dbf3e369379d1439636
Carolina <carolina.warntorp@outlook.com>
2026-09-18 16:43:42 +0200

    build: add first layout header

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="w-full flex justify-between">
+      <section>Nocturne</section>
+      <section>Menu/Profile</section>
+    </header>
+  );
+};
