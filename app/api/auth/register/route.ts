0513b3c4116930596b3bd062f7bb89d3f04d7cbe
StevenLomon <steven.cesario@proton.me>
2026-09-18 14:43:06 +0200

    body data validation

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,4 +4,18 @@ import { createClient } from '@/lib/supabase/server';
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, password } = body; // Bör diskuteras i gruppen ifall mer ska frågas efter vid sign up! Email och password får now, kan komma att ändras
+
+  // Basic validering for now:
+  // * lösenord måste vara minst 8 karaktärer
+  // * zod skulle kunna användas för att fånga felaktiga email format innan det skickas till Supabase! (som kommer validera om jag förstår rätt)
+  if (!email || !password) {
+    return NextResponse.json({ error: 'Email och lösenord krävs' }, { status: 400 });
+  }
+
+  if (password.length < 8) {
+    return NextResponse.json(
+      { error: 'Lösenordet måste vara minst 8 karaktärer långt' },
+      { status: 400 }
+    );
+  }
 }
