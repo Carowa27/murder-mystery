6b94ee7a24af8526ea74ca42249ea742400b5b3d
Carolina <carolina.warntorp@outlook.com>
2026-09-18 16:44:00 +0200

    build: add header and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -20,7 +22,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col px-4 pt-2">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
