fc7786e4d285101abdd1fe9c084a60c3c413e23f
Carolina <carolina.warntorp@outlook.com>
2026-09-20 12:38:13 +0200

    ref: upd layout header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="w-full flex justify-between">
+    <header className="w-full h-[64px] flex justify-between px-4 py-2 bg-background text-text-primary position-absolute top-0 sticky z-1000 ">
       <section>Nocturne</section>
       <section>Menu/Profile</section>
     </header>
