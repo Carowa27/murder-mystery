b746362e4c9374d909b93660e59e0f09db5db0c3
Carolina <carolina.warntorp@outlook.com>
2026-09-20 16:20:01 +0200

    ref: add colors to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       lang="en"
       className={`${caveat.variable} ${averageSans.variable} ${syneMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full max-w-3xl flex flex-col p-0 mx-auto">
+      <body className="min-h-full max-w-3xl flex flex-col p-0 mx-auto bg-background text-text-primary">
         <Header />
         <main className="px-2">{children}</main>
         <PageFoot />
