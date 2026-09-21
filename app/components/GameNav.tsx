24f28bc5e6a0a51200e5f94be62b710ad97b2904
Carolina <carolina.warntorp@outlook.com>
2026-09-21 15:15:27 +0200

    ref: upd links in game nav

--- app/components/GameNav.tsx
+++ app/components/GameNav.tsx
@@ -1,22 +1,27 @@
+'use client';
 import Link from 'next/link';
 import { MagnifyingGlassIcon, BooksIcon, UsersThreeIcon, LockIcon } from '@phosphor-icons/react';
+import { useParams } from 'next/navigation';
 
 export const GameNav = () => {
+  const params = useParams();
+  const investigationId = params.investigationId as string;
+  const baseUrl = `/investigation/${investigationId}`;
   return (
     <nav className="w-full h-[80px] flex justify-evenly mt-auto items-center position-absolute bottom-0 sticky z-1000 bg-background text-primary">
-      <Link href={'/office'} className="flex flex-col items-center gap-1">
+      <Link href={`${baseUrl}/office`} className="flex flex-col items-center gap-1">
         <MagnifyingGlassIcon size={32} weight="duotone" />
         <p className="!text-[0.70rem]"> OFFICE </p>
       </Link>
-      <Link href={'/evidence'} className="flex flex-col items-center gap-1">
+      <Link href={`${baseUrl}/evidence`} className="flex flex-col items-center gap-1">
         <BooksIcon size={32} weight="duotone" />
         <p className="!text-[0.70rem]"> EVIDENCE </p>
       </Link>
-      <Link href={'/team'} className="flex flex-col items-center gap-1">
+      <Link href={`${baseUrl}/team`} className="flex flex-col items-center gap-1">
         <UsersThreeIcon size={32} weight="duotone" />
         <p className="!text-[0.70rem]"> TEAM </p>
       </Link>
-      <Link href={'/accusation'} className="flex flex-col items-center gap-1">
+      <Link href={`${baseUrl}/accusation`} className="flex flex-col items-center gap-1">
         <LockIcon size={32} weight="duotone" />
         <p className="!text-[0.70rem]"> ACCUSE </p>
       </Link>
