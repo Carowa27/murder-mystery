771aab94ccd1b11a207098a05d8d63e279422a58
StevenLomon <steven.cesario@proton.me>
2026-09-21 15:15:13 +0200

    build: function skeleton

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,2 +1,11 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
+
+export async function POST(request: Request) {
+  // Ta emot body
+  // Skapa Supabase Client
+  // Kalla på `supabase.auth.signInWithPassword`
+  // Error hantering
+
+  return NextResponse.json({ test: 'test' }, { status: 201 });
+}
