f9bee0b9690fcf6e3436725c1b8aed0a822cd983
Carolina <carolina.warntorp@outlook.com>
2026-09-21 15:18:15 +0200

    ref: upd links in polaroid

--- app/components/Polaroid.tsx
+++ app/components/Polaroid.tsx
@@ -20,7 +20,7 @@ export const Polaroid = ({ c }: IPolaroidParam) => {
   const pathname = usePathname();
   return (
     <>
-      {pathname === '/office' ? (
+      {pathname.includes('/office') ? (
         <div className="shadow-sm relative bg-paper p-1 rounded-xs flex flex-col items-center aspect-[1/1.215]">
           <PushPinIcon size={17} color="#ca220c" weight="fill" className="z-1000 absolute -top-1" />
           {c.is_victim && (
