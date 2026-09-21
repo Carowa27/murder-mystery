fe866d7c6a0407f5ecf2c242766f82839f338147
Carolina <carolina.warntorp@outlook.com>
2026-09-21 10:47:00 +0200

    build: first layout character overview

---
+++ app/characters/page.tsx
@@ -0,0 +1,52 @@
+import { Polaroid } from '../components/Polaroid';
+
+const CharacterPage = () => {
+  const involvedPeople = [
+    {
+      first_name: 'Rosa',
+      last_name: 'Pantern',
+      relationship: 'panter hallucination',
+      description: 'tydlig hallucination som borde lämnat Clouseau för länge sedan',
+      image_url: null,
+      is_guilty: false,
+      is_victim: false,
+    },
+    {
+      first_name: 'Hercule',
+      last_name: 'Poirot',
+      relationship: 'detektiv',
+      description: 'ser allt, glömmer inget',
+      image_url: null,
+      is_guilty: false,
+      is_victim: false,
+    },
+    {
+      first_name: 'Jacques',
+      last_name: 'Clouseau',
+      relationship: 'polis',
+      description: 'klumpig och naiv fransk polis',
+      image_url: null,
+      is_guilty: false,
+      is_victim: true,
+    },
+    {
+      first_name: 'Jack',
+      last_name: 'Gaston',
+      relationship: 'Tjuvjägare',
+      description: 'Kvinnokarl',
+      image_url: null,
+      is_guilty: true,
+      is_victim: false,
+    },
+  ];
+  return (
+    <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/backgrounds/character-overview-bg.png)] bg-center bg-no-repeat bg-cover">
+      <section className="grid grid-cols-1 gap-2 mx-2 grid-cols-3">
+        {involvedPeople.map((p, i) => (
+          <Polaroid c={p} key={i} />
+        ))}
+      </section>
+    </div>
+  );
+};
+export default CharacterPage;
