b03e32ade239bad150399d13dcc562baf00d3798
Carolina <carolina.warntorp@outlook.com>
2026-09-23 11:02:24 +0200

    ref: breakout interface

---
+++ lib/interfaces/characters.ts
@@ -0,0 +1,8 @@
+export interface IGameCharacter {
+  first_name: string;
+  last_name: string;
+  relationship: string;
+  description: string;
+  image_url: string | null;
+  is_victim: boolean;
+}
