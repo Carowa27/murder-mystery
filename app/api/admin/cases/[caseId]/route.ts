1ee4c7d84b8cd094be3b28f5da8c4d91705666c0
Carolina <carolina.warntorp@outlook.com>
2026-09-23 16:36:50 +0200

    build: add admin cases and caseId routes

---
+++ app/api/admin/cases/[caseId]/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server';
+import { createClient } from '@/lib/supabase/server';
+
+export async function GET(request: Request, { params }: { params: Promise<{ caseId: string }> }) {
+  const { caseId } = await params;
+  try {
+    const supabase = await createClient();
+
+    if (!supabase) {
+      return NextResponse.json({ error: 'Failed to initialize Supabase client' }, { status: 500 });
+    }
+
+    const { data, error } = await supabase
+      .from('cases')
+      .select(
+        `
+      *,
+      difficulties (*),
+      characters (*),
+      case_clues (
+        *,
+        clue_types (*)
+    )`
+      )
+      .eq('id', caseId)
+      .single();
+
+    if (error || !data) {
+      return NextResponse.json(
+        { error: 'Case not found, error msg:' + error?.message },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error instanceof Error ? error.message : 'Internal server error',
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/api/admin/cases/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import { createClient } from '@/lib/supabase/server';
+
+export async function GET() {
+  try {
+    const supabase = await createClient();
+
+    if (!supabase) {
+      return NextResponse.json({ error: 'Failed to initialize Supabase client' }, { status: 500 });
+    }
+
+    const { data, error } = await supabase.from('cases').select(`
+        *,
+        difficulties (*),
+        characters (*),
+        case_clues (
+          *,
+          clue_types (*)
+        )`);
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 404 });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error instanceof Error ? error.message : 'Internal server error',
+      },
+      { status: 500 }
+    );
+  }
+}
