54fec3f2490c103210ba5af22ab914d3ddacfd9a
Carolina <carolina.warntorp@outlook.com>
2026-09-23 11:01:59 +0200

    ref: upd notebook layout

--- app/components/Notebook.tsx
+++ app/components/Notebook.tsx
@@ -2,17 +2,19 @@
 
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import notebook from '@/public/images/item-backgrounds/notebookv2.png';
 
 export const Notebook = () => {
   const params = useParams();
   const investigationId = params.investigationId as string;
   const baseUrl = `/investigation/${investigationId}`;
-
+  console.log(notebook.height, notebook.width);
   return (
-    <Link href={`${baseUrl}/notes`}>
-      <section className="h-[25%] mb-15 aspect-[1227/719] bg-[url(/images/item-backgrounds/notebook.png)] bg-bottom bg-no-repeat bg-cover">
-        {/* <p className="!text-xl text-surface !font-bold -rotate-8 mt-5 ml-15">case notes</p> */}
-      </section>
+    <Link
+      href={`${baseUrl}/notes`}
+      className="w-screen aspect-[1247/493] bg-[url(/images/item-backgrounds/notebookv2.png)] bg-bottom bg-no-repeat bg-cover"
+    >
+      {/* <p className="!text-xl text-surface !font-bold -rotate-8 mt-5 ml-15">case notes</p> */}
     </Link>
   );
 };
