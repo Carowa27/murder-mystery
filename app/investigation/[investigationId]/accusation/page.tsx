41972eeb28dcd1d1462ac7c3dc4d8bedc478fd4e
Carolina <carolina.warntorp@outlook.com>
2026-09-23 22:58:06 +0200

    ref: upd accusation bg link

--- app/investigation/[investigationId]/accusation/page.tsx
+++ app/investigation/[investigationId]/accusation/page.tsx
@@ -1,6 +1,6 @@
 const InvestigationPage = () => {
   return (
-    <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/background-images/accusation-bg.png)] bg-center bg-no-repeat bg-cover">
+    <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/backgrounds/accusation-bg.png)] bg-center bg-no-repeat bg-cover">
       Investigation
     </div>
   );
