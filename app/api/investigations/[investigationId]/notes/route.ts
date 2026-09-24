a5f2569c36940e9d3aa3da9b1643ece2519815e8
Carolina <carolina.warntorp@outlook.com>
2026-09-24 13:40:57 +0200

    ref: upd return from api

--- app/api/investigations/[investigationId]/notes/route.ts
+++ app/api/investigations/[investigationId]/notes/route.ts
@@ -15,7 +15,7 @@ export async function GET(
 
     const { data, error } = await supabase
       .from('notes')
-      .select('*')
+      .select('*,profiles(*)')
       .eq('investigation_id', investigationId);
 
     if (error) {
