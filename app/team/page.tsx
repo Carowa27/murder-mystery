2458bf708643e32522f698f8c5dc146490a875cd
StevenLomon <steven.cesario@proton.me>
2026-09-24 18:15:47 +0200

    build: file location with bg img

---
+++ app/team/page.tsx
@@ -0,0 +1,6 @@
+export default function TeamPage() {
+  return (
+    <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/backgrounds/team-bg.png)] bg-center bg-no-repeat bg-cover">
+    </div>
+  );
+}
