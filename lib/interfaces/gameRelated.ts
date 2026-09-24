ad761e71b349a356dc06472cf7ca0e3ffef5d315
Carolina <carolina.warntorp@outlook.com>
2026-09-24 13:09:03 +0200

    ref: filename add INotes

--- lib/interfaces/gameRelated.ts
+++ lib/interfaces/gameRelated.ts
@@ -6,3 +6,11 @@ export interface IGameCharacter {
   image_url: string | null;
   is_victim: boolean;
 }
+export interface INotes {
+  clue_id: string | null;
+  content: string;
+  created_at: string;
+  id: string;
+  investigation_id: string;
+  user_id: string;
+}
