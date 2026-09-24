020646952531df2668c5cc5a3232377cd05644b5
Carolina <carolina.warntorp@outlook.com>
2026-09-24 23:46:26 +0200

    ref: remove mock data and fetch correct

--- app/investigation/[investigationId]/accusation/page.tsx
+++ app/investigation/[investigationId]/accusation/page.tsx
@@ -1,62 +1,24 @@
 import { AccusationPhoto } from '@/app/components/AccusationPhoto';
+import { IGameCharacter } from '@/lib/interfaces/gameRelated';
+import { cookies } from 'next/headers';
 
-const InvestigationPage = () => {
-  const characters = [
+const InvestigationPage = async ({ params }: { params: Promise<{ investigationId: string }> }) => {
+  const { investigationId } = await params;
+  const cookieStore = await cookies();
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/investigations/${investigationId}/office`,
     {
-      first_name: 'Rosa',
-      last_name: 'Pantern',
-      relationship: 'panter hallucination',
-      description: 'tydlig hallucination som borde lämnat Clouseau för länge sedan',
-      image_url: null,
-      is_guilty: false,
-      is_victim: false,
-    },
-    {
-      first_name: 'Jack',
-      last_name: 'Gaston',
-      relationship: 'Tjuvjägare',
-      description: 'Kvinnokarl',
-      image_url: null,
-      is_guilty: false,
-      is_victim: false,
-    },
-    {
-      first_name: 'Hercule',
-      last_name: 'Poirot',
-      relationship: 'detektiv',
-      description: 'ser allt, glömmer inget',
-      image_url: null,
-      is_guilty: false,
-      is_victim: false,
-    },
-    {
-      first_name: 'Jacques',
-      last_name: 'Clouseau',
-      relationship: 'polis',
-      description: 'klumpig och naiv fransk polis',
-      image_url: null,
-      is_guilty: false,
-      is_victim: true,
-    },
-    {
-      first_name: 'Jack',
-      last_name: 'Gaston',
-      relationship: 'Tjuvjägare',
-      description: 'Kvinnokarl',
-      image_url: null,
-      is_guilty: true,
-      is_victim: false,
-    },
-    {
-      first_name: 'Jack',
-      last_name: 'Gaston',
-      relationship: 'Tjuvjägare',
-      description: 'Kvinnokarl',
-      image_url: null,
-      is_guilty: false,
-      is_victim: false,
-    },
-  ];
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: 'no-store',
+    }
+  );
+
+  const data = await res.json();
+  const characters: IGameCharacter[] = data.characters;
+
   return (
     <div className="min-h-[calc(100vh-64px-80px)] flex bg-[url(/images/backgrounds/accusation-bg.png)] bg-center bg-no-repeat bg-cover">
       <section className="w-[90%] flex flex-wrap self-end justify-center items-center gap-2 mx-auto mb-10">
