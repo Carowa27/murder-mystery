490eb40be4833057a92dbb5c7ee5a4b82d2db386
Carolina <carolina.warntorp@outlook.com>
2026-09-24 23:27:03 +0200

    build: add team data interfaces

--- lib/interfaces/gameRelated.ts
+++ lib/interfaces/gameRelated.ts
@@ -47,3 +47,46 @@ export interface IFoundClues {
   };
   found_at: string;
 }
+export interface ITeamSession {
+  id: string;
+  team_id: string;
+  case_id: string;
+  cases: ICase;
+  status: string;
+  started_at: string;
+  ended_at: string | null;
+  teams: ITeam;
+}
+
+export interface ITeam {
+  id: string;
+  name: string;
+  owner_id: string;
+  created_at: string;
+  invite_code: string;
+  max_members: number;
+  team_members: ITeamMember[];
+}
+
+export interface ITeamMember {
+  profiles: IProfile;
+  joined_at: string;
+}
+
+export interface IProfile {
+  id: string;
+  avatar_url: string | null;
+  display_name: string;
+}
+export interface ICase {
+  created_at: string;
+  description: string;
+  difficulty_id: number;
+  id: string;
+  image_url: string;
+  location: string;
+  price: number;
+  stage: 'active' | 'inactive';
+  story_date: string;
+  title: string;
+}
