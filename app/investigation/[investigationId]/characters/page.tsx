5dbbda8c457c975301f38bd4b5febcbf5a9b0a77
Carolina <carolina.warntorp@outlook.com>
2026-09-24 21:16:23 +0200

    ref: upd after polaroid comp upd

--- app/investigation/[investigationId]/characters/page.tsx
+++ app/investigation/[investigationId]/characters/page.tsx
@@ -1,9 +1,11 @@
 import { Polaroid } from '@/app/components/Polaroid';
 import { IGameCharacter } from '@/lib/interfaces/gameRelated';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
 const CharacterPage = async ({ params }: { params: Promise<{ investigationId: string }> }) => {
   const { investigationId } = await params;
+  const baseUrl = `/investigation/${investigationId}`;
   const cookieStore = await cookies();
 
   const res = await fetch(
@@ -23,9 +25,20 @@ const CharacterPage = async ({ params }: { params: Promise<{ investigationId: st
     <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/backgrounds/character-overview-bg.png)] bg-center bg-no-repeat bg-cover">
       <section className="flex flex-wrap justify-center gap-x-2 gap-y-5 mx-2 pt-[16%]">
         {/* <section className="grid grid-flow-col auto-cols-max gap-2 justify-center mx-2 pt-[16%]"> */}
-        {characters.map((p, i) => (
-          <Polaroid c={p} key={i} />
-        ))}
+        {characters &&
+          characters.map((p, i) => (
+            <Link key={i} href={`${baseUrl}/characters/${p.id}`}>
+              <Polaroid
+                c={p}
+                key={i}
+                showName={true}
+                showVictim={true}
+                onWall={true}
+                width={100}
+                crossSize={'big'}
+              />
+            </Link>
+          ))}
       </section>
     </div>
   );
