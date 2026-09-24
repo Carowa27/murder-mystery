f9857a6dcfa485f66eea3a61f370f09b0ee36746
Carolina <carolina.warntorp@outlook.com>
2026-09-24 13:09:43 +0200

    ref: upd import

--- app/investigation/[investigationId]/office/page.tsx
+++ app/investigation/[investigationId]/office/page.tsx
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 
 import { Notebook } from '@/app/components/Notebook';
 import { Polaroid } from '@/app/components/Polaroid';
-import { IGameCharacter } from '@/lib/interfaces/characters';
+import { IGameCharacter } from '@/lib/interfaces/gameRelated';
 
 const OfficePage = async ({ params }: { params: Promise<{ investigationId: string }> }) => {
   const { investigationId } = await params;
