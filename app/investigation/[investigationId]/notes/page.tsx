fbc26877ddc4c998e90b6b15592108c483d0d888
Carolina <carolina.warntorp@outlook.com>
2026-09-24 21:19:06 +0200

    ref: safe up with conditional

--- app/investigation/[investigationId]/notes/page.tsx
+++ app/investigation/[investigationId]/notes/page.tsx
@@ -21,9 +21,7 @@ const EvidencePage = async ({ params }: { params: Promise<{ investigationId: str
     <div className="min-h-[calc(100vh-64px-80px)] bg-[url(/images/backgrounds/evidence-bg.png)] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center">
       <div className={`mt-10 w-[calc(0.95*100%)] h-screen`}>
         <nav className="text-surface flex flex-col ps-10 pt-12 rotate-1 leading-5.5">
-          {notes.map((note, i) => (
-            <Note n={note} key={i} />
-          ))}
+          {notes && notes.map((note, i) => <Note n={note} key={i} />)}
         </nav>
       </div>
     </div>
